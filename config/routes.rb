36d35defcc30af7639f58376bf4c57a172e795b2
ryotaro3983 <110594547+ryotaro3983@users.noreply.github.com>
2022-08-24 11:39:42 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do          # ←ルーティングのネスト
+    resources :messages, only: [:index, :create]     #どのルームで投稿されたメッセなのかをパスから判断できるようにする
+  end
 end
